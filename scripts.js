8e03f3aed0cdb137466ddc05775ed31b9ab5e25d
PucheCarlos <puchecarlos00@gmail.com>
2024-06-04 21:36:14 -0400

    End proyect

--- scripts.js
+++ scripts.js
@@ -6,6 +6,9 @@ const date = new Date();
 const dia = date.getDay();
 const mediaQuery = window.matchMedia('(min-width: 450px)');
 let divisor;
+
+
+//The dimensions of the screen are checked to adjust the size of the graph
 if (mediaQuery.matches) {
   divisor = 7;
   console.log('La pantalla es mayor a 450px');
@@ -14,18 +17,17 @@ if (mediaQuery.matches) {
   console.log('La pantalla es menor o igual a 450px');
 }
 
+
 fetch("./data.json")
 .then(response => response.json())
   .then(data => {
-    setTimeout(()=>{
+    setTimeout(()=>{//I use a setTimeout to ensure that the elements have already loaded by the time the script is executed
       let dataValue = Object.values(data)
       for (let cont = 0; cont < data.length; cont++) {
         let valor = dataValue[cont].amount;
         let proporsion = Math.trunc(valor);
         bars[cont].style.setProperty("height", `${proporsion/divisor}rem`);
         spans[cont].innerHTML = `$${valor}`
-       
-        console.log(proporsion );
     }
     }, 0);
   })
@@ -34,12 +36,9 @@ fetch("./data.json")
   });
 
 
-  if (dia == 0) {
+  if (dia == 0) {//I get the current day to apply the distinctive color and adjust the data assignment
     bars[6].style.setProperty("background-color", "hsl(186, 34%, 60%)")
   } 
   else{
     bars[dia - 1].style.setProperty("background-color", "hsl(186, 34%, 60%)")
   }
-
-const elemento = document.getElementById('miElemento');
-
